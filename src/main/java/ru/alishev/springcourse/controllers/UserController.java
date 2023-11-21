1083a80f2a18df64d1dd49eaab9af04fa9129486
Furgon17 <pawel.grackov@gmail.com>
2023-11-21 15:36:53 +0300

    Тут все работает. но без Requestparam

---
+++ src/main/java/ru/alishev/springcourse/controllers/UserController.java
@@ -0,0 +1,75 @@
+package ru.alishev.springcourse.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.dao.UserDAO;
+import ru.alishev.springcourse.models.User;
+
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+@RequestMapping("/people")
+public class UserController {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", userDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") @Valid User user,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
+        userDAO.save(user);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
+        userDAO.update(id, user);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userDAO.delete(id);
+        return "redirect:/people";
+    }
+}
---
+++ src/main/java/ru/alishev/springcourse/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package ru.alishev.springcourse.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.alishev.springcourse.models.User;
+
+import java.util.List;
+
+
+@Component
+public class UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional(readOnly = true)
+    public List<User> index() {
+        return entityManager.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public User show(Integer id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Transactional
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Transactional
+    public void update(Integer id, User updatedUser) {
+        User userToBeUpdated = entityManager.find(User.class, id);
+        userToBeUpdated.setName(updatedUser.getName());
+        userToBeUpdated.setAge(updatedUser.getAge());
+        userToBeUpdated.setEmail(updatedUser.getEmail());
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        User userToBeDeleted = entityManager.find(User.class, id);
+        entityManager.remove(userToBeDeleted);
+    }
+}
